a25ee99f8b9d6e6f58a22ce319a5827403898cce
Aleksandr Andreyko <marphalina@gmail.com>
2020-02-24 02:28:39 +0300

    реализовал пару функций

---
+++ lesson2/script.js
@@ -0,0 +1,38 @@
+// let a = parseInt(prompt('Укажите певрое число'));
+// let b = parseInt(prompt('Укажите второе число'));
+
+// function checkOperation(fOp, sOp) {
+//   if(fOp >= 0 && sOp >= 0) alert(fOp - sOp);
+//   else if(fOp <= 0 && sOp <= 0) alert(fOp * sOp);
+//   else alert(fOp + sOp);
+// }
+
+// checkOperation(a, b);
+
+// let a1 = parseInt(prompt('Укажите число от 0 до 15'));
+
+// switch(a1) {
+//   case a1 > 0:
+//     for(let i = a1; i < 16; i++) {
+//       alert(i);
+//     }
+//     break;
+//   default:
+//     alert('Число должно быть в диапозоне от 0 до 15!');
+// }
+
+function sum(a, b) {
+  return alert(a + b);
+}
+
+function diff(a, b) {
+  return alert(a - b);
+}
+
+function mult(a, b) {
+  return alert(a * b);
+}
+
+function divis(a, b) {
+  return alert(a / b);
+}
