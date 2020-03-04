1c00e08e368d6bc72bc10c532d6445f348fc1ef6
Aleksandr <marphalina@gmail.com>
2020-03-04 21:27:48 +0300

    ходилка готова

---
+++ lesson4/walking/config.js
@@ -0,0 +1,10 @@
+/**
+ * Объект с настройками игры.
+ * @property {init} rowsCount Количесво строк на карте.
+ * @property {init} colsCount Количесво колонок на карте.
+ */
+
+const config = {
+    rowsCount: 10,
+    colsCount: 10,
+};
